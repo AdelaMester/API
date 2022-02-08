202dd1e4551e640a621165e8b6c158eee18ba363
AdelaMester <adelamester@yahoo.co.uk>
2022-02-08 14:55:45 +0000

    Added adgroups.csv in the database

---
+++ functions.py
@@ -0,0 +1,27 @@
+import sqlite3
+import csv
+
+
+# Connect to the database
+conn = sqlite3.connect('files.db')
+
+# Create a cursor object to execute queries on the database
+cursor = conn.cursor()
+
+
+# Open the csv file, read it and save the content in the adgroup variable
+with open("adgroups.csv", "r") as data:
+    adgroup = csv.reader(data)
+
+    # SQL query to insert data into adgroup table
+    insert_content = "INSERT INTO adgroup (ad_group,campaign_id,alias,status) VALUES(?,?,?,?)"
+
+    # Import content of adgroups.csv into database
+    cursor.executemany(insert_content, adgroup)
+
+    #Commiting the changes
+    conn.commit()
+
+    #Closing the connection
+    conn.close()
+
