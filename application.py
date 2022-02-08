5e3f6a391969f65430a05978c4f1501fa67808c3
AdelaMester <adelamester@yahoo.co.uk>
2022-02-08 17:12:49 +0000

    Added authorization to access the end point

--- application.py
+++ application.py
@@ -1,15 +1,27 @@
 from flask import Flask, jsonify
 import sqlite3
 from flask_restx import Api, Resource
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 # Configure application and Api
 app = Flask(__name__)
-api = Api(app, authorizations=authorizations, version='1.0', title='Bidnamic', description='Top 10 Search Terms by ROAS for a campaign structure_value or adgroup alias.')
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+# Authentication required
+authorizations = {
+    'api': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-API-KEY'
+    }
+}
+api = Api(app, authorizations=authorizations, security='api', version='1.0', title='Bidnamic', description='Top 10 Search Terms by ROAS for a campaign structure_value or adgroup alias.')
 ns = api.namespace('Data', description='Top 10 Search Terms by ROAS')
 
 # Define campaign route and the placeholder for a structure value
 @ns.route("/campaign/<string:structure_value>")
 class Campaign(Resource):
+    @api.doc(security='api')
     def get(self, structure_value):
 
         """ Returns the Top 10 Search Terms by ROAS for a campaign structure_value """
