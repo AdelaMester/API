4b9bf8d0b1717f0e8dc099049449929f2e5b441f
AdelaMester <adelamester@yahoo.co.uk>
2022-02-08 18:12:23 +0000

    Added adgroup end point

--- application.py
+++ application.py
@@ -47,3 +47,36 @@ class Campaign(Resource):
                 return({'message': 'Missing authentication'}), 401
         else:
             return({'message':'Missing authentication header'}), 401
+
+
+# Define adgroup route and the placeholder for a alias
+@ns.route("/adgroup/<string:alias>")
+class Adgroup(Resource):
+    @api.doc(security=os.environ.get('secret'))
+    def get(self, alias):
+
+        """ Returns the Top 10 Search Terms by ROAS for a adgroup alias """
+
+        # Check if the header exists
+        if 'X-API-KEY' in request.headers:
+            # Check if the header has the specific value
+            if request.headers['X-API-KEY'] == os.environ.get('secret'):
+
+                # Connect to the database
+                conn = sqlite3.connect('files.db')
+
+                # Create a cursor object to execute queries on the database
+                cursor = conn.cursor()
+
+                # Query the database to display the top 10 search terms for a adgroup alias
+                cursor.execute("SELECT DISTINCT conversion_value / cost AS roas, search_terms.search_term FROM search_terms JOIN adgroup ON search_terms.campaign_id = adgroup.campaign_id WHERE conversion_value !=0 AND cost != 0 AND adgroup.alias=? ORDER BY roas DESC LIMIT 10", (alias,))
+                topTen = cursor.fetchall()
+
+                # Closing the connection
+                conn.close()
+                return jsonify(topTen)
+            else:
+                return({'message': 'Missing authentication'}), 401
+        else:
+            return({'message':'Missing authentication header'}), 401
+
